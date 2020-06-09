c3bd7087b088671c33ab6814e648422371244dda
denisalis666@gmail.com <ujkjdfxtd382>
2020-06-09 18:51:22 +0300

    weeck 4

---
+++ Week/Week4/Периметр треугольника.py
@@ -0,0 +1,16 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+x3 = float(input())
+y3 = float(input())
+
+
+def distance(x1, y1, x2, y2, x3, y3):
+    a = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    b = ((x3 - x1) ** 2 + (y3 - y1) ** 2) ** 0.5
+    c = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
+    return a + b + c
+
+
+print(distance(x1, y1, x2, y2, x3, y3))
